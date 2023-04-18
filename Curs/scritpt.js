d449cb656f7fce9ff26b9393a79485793b4bebfe
SkyNikola <Skynikolai07@gmail.com>
2023-04-18 18:47:53 +0200

    <First commit>

---
+++ Curs/scritpt.js
@@ -0,0 +1,8 @@
+"use strict";
+const result = confirm("Are you here?");
+console.log(result);
+
+const answer = +prompt("How old are you?");
+console.log(typeof answer);
+
+// document.write(answer)
