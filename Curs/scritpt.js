80565fdbfbdf5f4fca839d55930d4c75f66fda1b
SkyNikola <Skynikolai07@gmail.com>
2023-04-19 18:31:00 +0200

    Exercise.done

--- Curs/scritpt.js
+++ Curs/scritpt.js
@@ -1,18 +1,21 @@
 "use strict";
 
 let numberOfFilms = prompt("How many films have you watched?");
-let lastFilm = prompt("Your last film.");
-let rate = prompt("How much do you rate it?");
 
 let personalMoieDB = {
   count: numberOfFilms,
-  movies: {
-    lastFilm,
-    rate,
-  },
-  actors: "",
+  movies: {},
+  actors: {},
   genres: [],
   privat: false,
 };
+
+const a = prompt("Your last film."),
+  b = prompt("How much do you rate it?"),
+  c = prompt("Your last film."),
+  d = prompt("How much do you rate it?");
+
+personalMoieDB.movies[a] = b;
+personalMoieDB.movies[c] = d;
 
 console.log(personalMoieDB);
